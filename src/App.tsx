380ff065652717107aee3c6d03b3dc5ede29e216
Mohammad Al Zouabi <mb.alzouabi@gmail.com>
2024-01-04 14:51:43 +0800

    feat: header

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
-import { createSignal } from 'solid-js';
+function Header() {
+  return (
+    <header class="md:flex md:items-center md:justify-between bg-zinc-900 p-4">
+      <div class="min-w-0 flex-1">
+        <h2 class="text-2xl font-medium leading-7 text-white sm:truncate sm:text-3xl sm:tracking-tight">
+          Markdown to Confluence Wiki Markup Editor
+        </h2>
+      </div>
+    </header>
+  );
+}
 
 function App() {
-  const [count, setCount] = createSignal(0);
-
   return (
-    <div class="p-8 w-screen flex items-center justify-center">
-      <button onClick={() => setCount((count) => count + 1)}>Count is {count()}</button>
+    <div class="h-screen">
+      <Header />
     </div>
   );
 }
