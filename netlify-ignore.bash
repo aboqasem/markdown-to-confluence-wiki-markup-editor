551119a06115639d5497a306561d8c09aa04618c
Mohammad Al Zouabi <mb.alzouabi@gmail.com>
2024-01-08 20:40:20 +0800

    chore: add `netlify.toml` and build ignore condition

---
+++ netlify-ignore.bash
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Ignore builds triggered by changes to 'README.md', '.vscode/*', and/or '.github/*' ONLY
+# See: https://docs.netlify.com/configure-builds/ignore-builds/#custom-ignore-command
+! git diff --name-only $CACHED_COMMIT_REF $COMMIT_REF | grep --quiet --invert-match --extended-regexp '^README\.md|\.(vscode|github)/.*$'
