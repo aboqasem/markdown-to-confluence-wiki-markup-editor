190bd51df0ce7acc3b935ab844a3919d22ebd564
Mohammad Al Zouabi <mb.alzouabi@gmail.com>
2024-01-08 21:27:34 +0800

    chore: biome to use vcs & netlify ignore `biome.json`

--- netlify-ignore.bash
+++ netlify-ignore.bash
@@ -2,4 +2,5 @@
 
 # Ignore builds triggered by changes to 'README.md', '.vscode/*', and/or '.github/*' ONLY
 # See: https://docs.netlify.com/configure-builds/ignore-builds/#custom-ignore-command
-! git diff --name-only $CACHED_COMMIT_REF $COMMIT_REF | grep --quiet --invert-match --extended-regexp '^README\.md|\.(vscode|github)/.*$'
+! git diff --name-only "$CACHED_COMMIT_REF" "$COMMIT_REF" |
+  grep --quiet --invert-match --extended-regexp '^README\.md|biome.json|\.(vscode|github)/.*$'
