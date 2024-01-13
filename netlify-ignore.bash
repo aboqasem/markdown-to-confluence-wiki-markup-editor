173627dad5f6716567524e18dfb42d628cb688fb
Mohammad Al Zouabi <mb.alzouabi@gmail.com>
2024-01-13 15:34:12 +0000

    feat(dx): bun.lockb diffing & codesandbox + devcontainer config

--- netlify-ignore.bash
+++ netlify-ignore.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Ignore builds triggered by changes to 'README.md', '.vscode/*', and/or '.github/*' ONLY
+# Ignore builds triggered by changes to unrelated files only
 # See: https://docs.netlify.com/configure-builds/ignore-builds/#custom-ignore-command
 ! git diff --name-only "$CACHED_COMMIT_REF" "$COMMIT_REF" |
-  grep --quiet --invert-match --extended-regexp '^README\.md|biome.json|\.(vscode|github)/.*$'
+  grep --quiet --invert-match --extended-regexp '^README\.md|biome\.json|\.git(attributes|config)|\.(vscode|github|codesandbox|devcontainer)/.*$'
