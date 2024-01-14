14b9e8e0bda35e18b3cfab56fc27aef1890594eb
Mohammad Al Zouabi <mb.alzouabi@gmail.com>
2024-01-14 19:25:07 +0800

    test: faster test & upgrade jsdom

--- src/components/converter.test.tsx
+++ src/components/converter.test.tsx
@@ -3,31 +3,35 @@ import "@testing-library/jest-dom";
 import userEvent from "@testing-library/user-event";
 import { Converter } from "~/components/converter";
 
+function assertInstanceOf<T>(value: unknown, type: new () => T): T {
+  chai.assert.instanceOf(value, type);
+
+  return value as T;
+}
+
 describe("Converter", () => {
   const converterId = "converter";
   const converterInputId = `${converterId}_input-editor`;
   const converterOutputId = `${converterId}_output-editor`;
   let rendered: ReturnType<typeof render>;
-  let inputEditor: HTMLElement;
-  let outputEditor: HTMLElement;
+  let inputEditor: HTMLDivElement;
+  let outputEditor: HTMLDivElement;
 
   beforeEach(() => {
     rendered = render(() => <Converter id={converterId} />);
-    inputEditor = rendered
-      .getByTestId(converterInputId)
-      .querySelector(".cm-content") as HTMLElement;
-    outputEditor = rendered
-      .getByTestId(converterOutputId)
-      .querySelector(".cm-content") as HTMLElement;
-  });
-
-  it("exists", () => {
-    expect(inputEditor).toBeInTheDocument();
-    expect(outputEditor).toBeInTheDocument();
+    inputEditor = assertInstanceOf(
+      rendered.getByTestId(converterInputId).querySelector(".cm-content"),
+      HTMLDivElement,
+    );
+    outputEditor = assertInstanceOf(
+      rendered.getByTestId(converterOutputId).querySelector(".cm-content"),
+      HTMLDivElement,
+    );
   });
 
   it("converts markdown to confluence wiki markup", async () => {
-    const input = `# Hello World
+    const input = `
+# Hello World
 
 - This is a list item
 - This is another list item
@@ -40,7 +44,7 @@ console.log("Hello World");
 \`\`\`
 `;
 
-    await userEvent.type(inputEditor, input);
+    await userEvent.type(inputEditor, input, { delay: null });
 
     expect(outputEditor).matchSnapshot();
   });
